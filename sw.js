5ec87edfad021a4a9cc95150e3fafbaef9d90f52
ElFaraon65 <108295477+ElFaraon65@users.noreply.github.com>
2026-01-07 13:11:20 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,18 @@
 const CACHE_NAME = 'bio-breach-v1';
-// Lista de archivos que el celular guardará para que abran sin señal
+// Deja solo index.html para probar si arranca
 const assets = [
   './',
-  'index.html',
-  'versiones.json',
-  'manifest.json'
+  'index.html'
 ];
 
-// Instalación: El celular descarga los archivos básicos
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Hub: Guardando archivos en memoria local...');
       return cache.addAll(assets);
     })
   );
 });
 
-// Activación: Limpia memorias viejas si actualizas el Hub
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Intercepción: Si no hay señal, sirve los archivos desde la memoria
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
