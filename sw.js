9a978d2aef1d335b2198bfa49c9fff6e3fc67761
ElFaraon65 <108295477+ElFaraon65@users.noreply.github.com>
2026-01-20 14:54:33 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -69,8 +69,8 @@ self.addEventListener('push', (event) => {
   
   const options = {
     body: event.data ? event.data.text() : 'BIO-BREACH HUB: Nueva versión disponible',
-    icon: 'https://raw.githubusercontent.com/ElFaraon65/bio-breach-repositorio/main/Logo%20BIO-BREACH.png',
-    badge: 'https://raw.githubusercontent.com/ElFaraon65/bio-breach-repositorio/main/Logo%20BIO-BREACH.png',
+    icon: "./Logo%20BIO-BREACH.png",
+    badge: "./Logo%20BIO-BREACH.png",
     vibrate: [100, 50, 100],
     data: {
       dateOfArrival: Date.now(),
