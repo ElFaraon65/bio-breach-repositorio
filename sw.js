9be60cb8ae333b381012fe9d7badb1354c44e1b0
ElFaraon65 <108295477+ElFaraon65@users.noreply.github.com>
2026-09-24 21:27:46 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "bio-breach-v6";
-const DATA_CACHE = "bio-breach-db-v5"; // Base de datos para guardar hora de salida
+const DATA_CACHE = "bio-breach-db-v6"; // Base de datos para guardar hora de salida
 const ASSETS = [
   "./",
   "./index.html",
